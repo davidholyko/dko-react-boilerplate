c8ffeb604d7b67ad7ce6a105ed3157284f41dda0
davidholyko <david.holy.ko@gmail.com>
2019-04-29 18:23:13 -0400

    add alertlist, alert auto close after 2 seconds

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -9,7 +9,7 @@ import SignIn from '../auth/SignIn'
 import SignOut from '../auth/SignOut'
 import ChangePassword from '../auth/ChangePassword'
 
-import Alert from 'react-bootstrap/Alert'
+import { AlertList } from 'react-bs-notifier'
 
 class App extends Component {
   constructor () {
@@ -17,7 +17,9 @@ class App extends Component {
 
     this.state = {
       user: null,
-      alerts: []
+      alerts: [],
+      timeout: 2000,
+      position: 'bottom-left'
     }
   }
 
@@ -25,23 +27,41 @@ class App extends Component {
 
   clearUser = () => this.setState({ user: null })
 
-  alert = (message, type) => {
-    this.setState({ alerts: [...this.state.alerts, { message, type }] })
+  alert = (message, type, headline = '', timeout = 2000) => {
+    const newAlert = {
+      id: (new Date()).getTime(),
+      type: type,
+      headline: headline,
+      message: message
+    }
+
+    this.setState(prevState => ({
+      alerts: [...prevState.alerts, newAlert]
+    }), () => {
+      setTimeout(() => {
+        const index = this.state.alerts.indexOf(newAlert)
+        if (index >= 0) {
+          this.setState(prevState => ({
+            // remove the alert from the array
+            alerts: [...prevState.alerts.slice(0, index), ...prevState.alerts.slice(index + 1)]
+          }))
+        }
+      }, timeout)
+    })
   }
 
   render () {
-    const { alerts, user } = this.state
+    const { alerts, user, timeout, position } = this.state
 
     return (
       <React.Fragment>
         <Header user={user} />
-        {alerts.map((alert, index) => (
-          <Alert key={index} dismissible variant={alert.type}>
-            <Alert.Heading>
-              {alert.message}
-            </Alert.Heading>
-          </Alert>
-        ))}
+
+        <AlertList
+          position={position}
+          alerts={alerts}
+          timeout={timeout}
+        />
         <main className="container">
           <Route path='/sign-up' render={() => (
             <SignUp alert={this.alert} setUser={this.setUser} />
